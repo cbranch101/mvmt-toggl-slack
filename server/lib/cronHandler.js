f23fdffff3af4ad5a780bcfd895dafa2e68b99c4
Clay Branch <clay@movementstrategy.com>
2015-09-02 16:34:25 -0600

    fixed cron job frequency

--- server/lib/cronHandler.js
+++ server/lib/cronHandler.js
@@ -8,8 +8,7 @@ Meteor.methods({
 			name: 'send slack messages',
 			schedule: function(parser) {
 				// parser is a later.parse object
-// 				return parser.text('at 7:00am every weekday');
-				return parser.text('every 5 seconds');
+				return parser.text('at 7:00am every weekday');
 			},
 			job: function() {
 				togglHandler.generateSlackMessagesForTrackedUsers();
